9af9c011224e527e2b705b86e64905b42f187918
Hamid <www.abdulhamid3311@gmail.com>
2023-04-10 14:50:28 +0600

    sol UI updated

--- components/Pages/Solutions/Solutions.js
+++ components/Pages/Solutions/Solutions.js
@@ -50,22 +50,22 @@ const SolutionCard = ({ singleSol }) => {
 		<div>
 			<h2 className="text-2xl font-bold mb-5">{title}</h2>
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-10 text-primary">
-				<div className="w-full bg-white rounded-md  p-5 text-center">
+				<div className="w-full bg-white rounded-md  p-5 text-center border-t-8 border-greenclr">
 					<div className="text-5xl flex items-center justify-center ">{feat1Icon}</div>
-					<h2 className="text-2xl font-semibold my-5 text-greenclr">{feat1}</h2>
-					<p>{feat1Desc}</p>
+					<h2 className="text-2xl font-semibold mt-5 mb-3 text-greenclr">{feat1}</h2>
+					<p className=" text-paraclr">{feat1Desc}</p>
 				</div>
 
-				<div className="w-full bg-white  p-5 rounded-md  text-center">
+				<div className="w-full bg-white  p-5 rounded-md  text-center  border-t-8 border-greenclr">
 					<div className="text-5xl flex items-center justify-center ">{feat2Icon}</div>
-					<h2 className="text-2xl font-semibold my-5 text-greenclr">{feat2}</h2>
-					<p>{feat2Desc}</p>
+					<h2 className="text-2xl font-semibold mt-5 mb-3 text-greenclr">{feat2}</h2>
+					<p className=" text-paraclr">{feat2Desc}</p>
 				</div>
 
-				<div className="w-full bg-white  p-5 rounded-md  text-center">
+				<div className="w-full bg-white  p-5 rounded-md  text-center  border-t-8 border-greenclr">
 					<div className="text-5xl flex items-center justify-center ">{feat3Icon}</div>
-					<h2 className="text-2xl font-semibold my-5 text-greenclr">{feat3}</h2>
-					<p>{feat3Desc}</p>
+					<h2 className="text-2xl font-semibold mt-5 mb-3 text-greenclr">{feat3}</h2>
+					<p className=" text-paraclr">{feat3Desc}</p>
 				</div>
 			</div>
 		</div>
