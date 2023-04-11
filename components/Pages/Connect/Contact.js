76cb8f2bb266e7d9fd5629f10098e32e5cf61c43
Hamid <www.abdulhamid3311@gmail.com>
2023-04-11 14:46:41 +0600

    solved design issue

--- components/Pages/Connect/Contact.js
+++ components/Pages/Connect/Contact.js
@@ -11,15 +11,15 @@ const Contact = () => {
 	return (
 		<div className="">
 			<div className="banner-bg flex items-center justify-center">
-				<h2 className="text-6xl font-bold text-center text-white">
+				<h2 className="text-4xl lg:text-6xl font-bold text-center text-white">
 					Contact The AuthBrain
 				</h2>
 			</div>
 
-			<div className="p-10 max-w-7xl mx-auto flex items-center justify-between gap-10 px-6 lg:px-32">
+			<div className="p-10 max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-10 px-6 lg:px-32">
 				<div>
-					<div className="text-start mt-14">
-						<h3 className="text-greenclr mb-2 font-bold">Registered Address</h3>
+					<div className="text-start">
+						<h3 className="text-greenclr mb-2 font-bold">Address</h3>
 						<p className="">
 							Deira, Near Gold Center Building,
 							<br />
@@ -28,7 +28,7 @@ const Contact = () => {
 						</p>
 					</div>
 
-					<div className="text-start mt-14">
+					<div className="text-start mt-10">
 						<h3 className="text-greenclr mb-2 font-bold">Contact</h3>
 
 						<div className="flex flex-col gap-4">
@@ -88,7 +88,6 @@ const Contact = () => {
 						src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3607.9847392458055!2d55.29374612695312!3d25.271098800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sDeira%2C%20Near%20Gold%20Center%20Building%2C%20Dubai%2075157%2C%20United%20Arab%20Emirates!5e0!3m2!1sen!2snl!4v1681202182863!5m2!1sen!2snl"
 						width="600"
 						height="450"
-						
 						allowfullscreen=""
 						loading="lazy"
 						referrerpolicy="no-referrer-when-downgrade"
